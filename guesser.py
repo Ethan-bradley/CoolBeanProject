2b727bf91210c6a412f7433cdac3001c681344a4
anthonyebiner <anthonyebiner@gmail.com>
2019-10-26 20:16:06 -0700

    convert guesser to weeks

--- guesser.py
+++ guesser.py
@@ -2,11 +2,11 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 
-model = tf.keras.models.load_model('model2.h5')
+model = tf.keras.models.load_model('model3.h5')
 
-def guess(days, time, tweets):
-    days = (days - 407.633877) / 186.525372
+def guess(weeks, time, tweets):
+    weeks = (weeks - 57.795247) / 26.644229
     time = (time - 3.531709) / 2.064484
     tweets = (tweets - 62.426499) / 51.817219
-    df = pd.DataFrame.from_dict({'Days': [days], 'Time': [time], 'tweets': [tweets]})
+    df = pd.DataFrame.from_dict({'Days': [weeks], 'Time': [time], 'tweets': [tweets]})
     return model.predict(np.array(df))[0][0]
