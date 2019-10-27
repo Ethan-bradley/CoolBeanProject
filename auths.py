f125f419447f324f69b6340a141da3fdc96b3310
Justin Chen <jdchen778@gmail.com>
2019-10-26 17:28:17 -0700

    Adding auths.py

---
+++ auths.py
@@ -0,0 +1,4 @@
+consumer_key = "YpwukJpgNwHgu9RNKlwSznkdQ"
+consumer_secret = "7gKBgNkb3RJzhiWr8SPTCi759W5mjp6v7lMIU72XSz0xi0RZRy"
+access_token = "1188239932340326401-Atbp2C5IamjrXC69mfZSOrYnlE3zWg"
+access_token_secret = "p3KZCoLRRbTSqSmpbWfxGxxG3nt9YGrTsas7hEJd584tq"
